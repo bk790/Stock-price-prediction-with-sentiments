24d1aebd791a64951274a7023a9e636d26824483
Bhanu Kumar <bhanukumar474@gmail.com>
2025-02-16 01:34:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JS to add interactivity to the "Get Started" button
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+    alert('Welcome to DataWorld! Let the journey begin!');
+});
