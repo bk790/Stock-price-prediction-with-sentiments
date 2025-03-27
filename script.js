667891012faf52ad1beade89043caf52b532f41f
Bhanu Kumar <bhanukumar474@gmail.com>
2025-03-27 20:44:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Simple JS to add interactivity to the "Get Started" button
-document.getElementById('getStartedBtn').addEventListener('click', function() {
-    alert('Welcome to DataWorld! Let the journey begin!');
-});
